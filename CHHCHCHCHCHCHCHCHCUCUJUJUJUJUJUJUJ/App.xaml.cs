f40cce5cada6d1bdbdae0fa6f2b8405b655b75d8
4m <witiuk61@gmail.com>
2024-10-31 08:41:39 +0100

    Dodaj pliki projektów.

---
+++ CHHCHCHCHCHCHCHCHCUCUJUJUJUJUJUJUJ/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CHHCHCHCHCHCHCHCHCUCUJUJUJUJUJUJUJ
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CHHCHCHCHCHCHCHCHCUCUJUJUJUJUJUJUJ/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace CHHCHCHCHCHCHCHCHCUCUJUJUJUJUJUJUJ
+{
+    public partial class MainPage : ContentPage
+    {
+        int cena = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void kupBilet(object sender, EventArgs e)
+        {
+            cena += 15;
+            Koszyk.Text = $"Cena za wszystkie bilety : {cena} zł";
+        }
+    }
+
+}
